d4d615ebd1c78c2172899f6cc2771195442d4e3f
zy <210849049@qq.com>
2020-10-25 22:09:56 +0800

    admin

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    //
+    public function showLoginForm(){
+        return view('admin.auth.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\UploadController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +23,18 @@ Route::get('/', function () {
 });
 Route::get('/blog', [BlogController::class,'index'])->name('blog.name');
 Route::get('/blog/{slug}', [BlogController::class,'showPost'])->name('blog.detail');
+
+Route::get('admin',function(){
+    return redirect('/admin/post');
+});
+
+Route::middleware('auth')->namespace('Admin')->group(function(){
+    Route::resource('admin/post', PostController::class);
+    Route::resource('admin/tag', TagController::class);
+    Route::get('admin/upload', [UploadController::class,'index']);
+});
+
+//登录退出
+Route::get('/login', [LoginController::class,'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class,'login']);
+Route::get('/logout', [LoginController::class,'logout'])->name('logout');
