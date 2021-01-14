0dbc2af73574640d6399b9a8b178db19dabc81cb
JJSama <210849049@qq.com>
2021-01-14 18:41:17 +0800

    88

--- public/blog_asset/js/scripts.js
+++ public/blog_asset/js/scripts.js
@@ -208,14 +208,12 @@ $(window).scroll(function () {
 $(function () {
     $('a[href*=#]').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var $target = $(this.hash);
+            let $target = $(this.hash);
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
             if ($target.length) {
-                var targetOffset = $target.offset().top;
-                $('html,body').animate({
-                        scrollTop: targetOffset
-                    },
-                    1000);
+                let targetOffset = $target.offset().top - 88;
+                console.log($target);
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
                 return false;
             }
         }
