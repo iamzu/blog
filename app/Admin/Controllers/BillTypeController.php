4cb3c6671fc100f59e9f5ab531f5cda1a4e5d9c9
JJSama <210849049@qq.com>
2021-04-19 17:54:26 +0800

    edit

--- app/Admin/Controllers/BillTypeController.php
+++ app/Admin/Controllers/BillTypeController.php
@@ -27,8 +27,8 @@ class BillTypeController extends AdminController
                 if($userId !== 1){
                     $tree->actions(function (Tree\Actions $actions) use ($userId) {
                         $actions->disableDelete(); // 禁用删除按钮
-                        if ($actions->row->user_id !== $userId) {
-                            $actions->disableEdit(); // 禁用删除按钮
+                        if ($actions->row->user_id != $userId) {
+                            $actions->disableEdit();
                         }
                     });
                 }
