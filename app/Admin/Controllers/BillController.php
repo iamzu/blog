dd4292588cad8b7ddf9183d2c6d5203d423478cd
JJSama <210849049@qq.com>
2021-04-19 18:06:05 +0800

    user_id

--- app/Admin/Controllers/BillController.php
+++ app/Admin/Controllers/BillController.php
@@ -24,31 +24,22 @@ class BillController extends AdminController
     {
         return Grid::make(new Bill(), function (Grid $grid) {
             $grid->model()->where('user_id',Admin::user()->id);
-            $grid->column('id')->sortable();
-//            $grid->column('user_id');
-            $grid->column('tag_id')->display(function () {
-                return $this->tags->tag;
-            })->label('info');
             $grid->column('type')->using([1 => '支出', 2 => '收入'])->label([
                 1 => 'default',
                 2 => 'danger',
             ]);
-
+            $grid->column('tag_id')->display(function () {
+                return $this->tags->tag;
+            })->label('info');
             $grid->column('money')->display(function ($val) {
                 return '￥' . $val;
-            });
+            })->sortable();
             $grid->column('remarks');
-            $grid->column('created_at', '创建/更新')->display(function () {
-                return <<<HTML
-            <span>{$this->created_at}</span>
-            <br>
-            <br>
-            <span>{$this->updated_at}</span>
-HTML;
+            $grid->column('created_at','日期')->display(function($val){
+                return date('Y-m-d',strtotime($val));
             });
             $grid->filter(function (Grid\Filter $filter) {
                 $filter->equal('id');
-
             });
         });
     }
@@ -82,25 +73,15 @@ HTML;
     protected function form()
     {
         return Form::make(new Bill(), function (Form $form) {
-            $form->display('id');
             $form->hidden('user_id')->value(Admin::user()->id);
 
             $form->select('parent_tag_id')
                 ->options(BillType::all()->where('level', 1)->pluck('tag', 'id'))->required()->load('tag_id', route('sub-bill-type'));
             $form->select('tag_id')->required();
-
             $form->radio('type')->options(['1' => '支出', '2' => '收入'])->default('1');
             $form->currency('money')->symbol('￥');
             $form->textarea('remarks');
-
-            $form->date('created_at')->default(Carbon::now()->format('Y-m-d'))->required();
-            $form->display('updated_at');
+            $form->date('created_at','日期')->default(Carbon::now()->format('Y-m-d'))->required();
         });
-    }
-
-    public function subBillType(Request $request)
-    {
-        $pid = $request->input('q');
-        dd($pid);
     }
 }
