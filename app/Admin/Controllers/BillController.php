e9f3847a5e7e19bc366d17c54c8e855585ac58af
JJSama <210849049@qq.com>
2021-04-23 17:00:41 +0800

    筛选

--- app/Admin/Controllers/BillController.php
+++ app/Admin/Controllers/BillController.php
@@ -9,12 +9,32 @@ use Carbon\Carbon;
 use Dcat\Admin\Admin;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
+use Dcat\Admin\Layout\Content;
+use Dcat\Admin\Layout\Row;
 use Dcat\Admin\Show;
 use Dcat\Admin\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Request;
 
 class BillController extends AdminController
 {
+
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     *
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->title($this->title())
+            ->description($this->description()['index'] ?? trans('admin.list'))
+            ->body(function (Row $row){
+                $row->column(12,$this->grid());
+            });
+    }
     /**
      * Make a grid builder.
      *
@@ -23,11 +43,30 @@ class BillController extends AdminController
     protected function grid()
     {
         return Grid::make(new Bill(), function (Grid $grid) {
+            $grid->filter(function (Grid\Filter $filter) {
+                $filter->scope(1, '本月')->whereBetween('created_at', [
+                    Carbon::now()->startOfMonth()->format('Y-m-d H:i:s'),
+                    Carbon::now()->endOfMonth()->format('Y-m-d H:i:s'),
+                ]);
+                $filter->scope(2, '上月')->whereBetween('created_at', [
+                    Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d H:i:s'),
+                    Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d H:i:s'),
+                ]);
+                // 更改为 panel 布局
+                $filter->panel();
+                $filter->equal('parent_tag_id','主类别')->select(BillType::all()->where('level', 1)->pluck('tag', 'id'));
+            });
+
             $grid->model()->where('user_id',Admin::user()->id)->orderBy('created_at','desc');
             $grid->column('type')->using([1 => '支出', 2 => '收入'])->label([
                 1 => 'default',
                 2 => 'danger',
-            ]);
+            ])->filter(
+                Grid\Column\Filter\In::make([
+                    1 => '支出',
+                    2 => '收入',
+                ])
+            );
             $grid->column('tag_id')->display(function () {
                 return $this->tags->tag;
             })->label('info');
